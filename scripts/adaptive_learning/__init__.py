0a579407a1837c5fe15dc23aa473971b30e2cfdd
colinbousige <colin.bousige@gmail.com>
2023-12-01 13:58:42 +0100

    updated __init__.py

--- scripts/adaptive_learning/__init__.py
+++ scripts/adaptive_learning/__init__.py
@@ -6,7 +6,7 @@ List of different functions to be called in an adaptive learning script to build
 - Author: Colin BOUSIGE
 - Email: colin.bousige@cnrs.fr
 - Affiliation: CNRS, [Laboratoire des Multimatériaux et Interfaces](http://lmi.cnrs.fr), Lyon, France
-- Date: 08/03/2023
+- Date: 18/04/2023
 
 ## Usage
 
@@ -15,13 +15,13 @@ First, add this folder (`$HOME/Boro_ML/bin/iteration_tools` or wherever you put 
 Then, go to your working directory, and make sure the following files are there:
 
 - Necessary:
-    - `stock.data`: input.data file with all stock structures
-    - `initial_input.data.`: initial input.data structures dataset
-    - `input1.nn`: input.nn for NNP1
-    - `input2.nn`: input.nn for NNP2
+    - `stock.data`: `input.data` file with all stock structures
+    - `initial_input.data.`: initial `input.data` structures dataset
+    - `input1.nn`: `input.nn` for NNP1
+    - `input2.nn`: `input.nn` for NNP2
 - Optional:
-    - `EW.data`: input.data file with only EW structures. They will be computed with VASP and added to the dataset at the beginning of the procedure.
-    - `input3.nn`: input.nn for NNP3
+    - `EW.data`: `input.data` file with only EW structures. They will be computed with VASP and added to the dataset at the beginning of the procedure. This file is created with the script `xLAMMPStoNNP`.
+    - `input3.nn`: `input.nn` for NNP3
     - etc
 
 Then just do:
@@ -31,6 +31,13 @@ xjobadaptive node26
 ```
 
 It will copy the python script `$HOME/Boro_ML/bin/iteration_tools/adaptive_training.py` in the current directory, and launch the script on `node26`.
+
+## Note
+
+`stock.data` is a file containing all the structures that will be used in the adaptive learning procedure. It is usually created with the script `xLAMMPStoNNP` that gathers all structures in a LAMMPS trajectory into an n2p2-type `stock.data` file. If created by this script, all energies and forces are set to 0 since they are not known at the DFT level.
+
+In case you have already computed some structures with DFT, you can still include them in the `stock.data` file. In this case, all structures that have non zero energies will be extracted to individual `vasp/OUTCAR_i.inp` files. This way, they will be detected as already computed and not be computed again during the adaptive learning procedure.
+
 
 ## List of available classes:
 
@@ -43,8 +50,8 @@ It will copy the python script `$HOME/Boro_ML/bin/iteration_tools/adaptive_train
 
 ## List of available functions:
 
-- `read_inputdata(filename: str)`
-    - Read `input.data` file and return a list of Atoms objects and the comments
+- `read_inputdata(filename: str, copy_data=None)`
+    - Read `input.data` file and return a list of Atoms objects and the comments. If `copy_data` is a path, it will copy the corresponding inp file to `copy_data/OUTCAR_i.inp` for all structures with non-zero energy and indices i.
 - `write_inputdata(filename: str, atoms, comments)`
     From a list of Atoms object and comments, write an `input.data` file to `filename`.
 - `print_success_message()`
